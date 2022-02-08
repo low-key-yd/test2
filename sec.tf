9686d0cfa3d7a537d1c19008106029428502e00e
low-key-yd <yesudas_test@outlook.com>
2022-02-08 22:08:40 +0000

    small_change

--- sec.tf
+++ sec.tf
@@ -57,3 +57,31 @@ resource "aws_security_group" "ssh_access" {
   }
 }
 
+resource "aws_security_group" "ssh_access_1" {
+
+  name        = "ssh_new"
+  description = "allow 22 traffic"
+
+
+  ingress {
+    description      = ""
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["112.1.1.1/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-${var.project}"
+  }
+}
+
