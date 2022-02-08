6643ce4345c532fafd1e3d87a5e9bcd79a36f2e4
low-key-yd <yesudas_test@outlook.com>
2022-02-08 21:40:50 +0000

    new

--- sec.tf
+++ sec.tf
@@ -57,3 +57,33 @@ resource "aws_security_group" "ssh_access" {
   }
 }
 
+
+resource "aws_security_group" "web11" {
+
+  name        = "web11"
+  description = "allows 80,443 traffic"
+
+
+  ingress {
+    description      = ""
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "web-${var.project}-security_group"
+  }
+}
+
